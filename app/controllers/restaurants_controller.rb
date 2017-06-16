dab20b3fc8c116b33b7629e461c5ed8226b24e8f
Lia Forgione <liaforg@gmail.com>
2017-06-16 14:04:22 -0400

    restaurant controller page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,10 +8,42 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
   def restaurant_params
     params.required(:restaurant).permit(:name, :url)
   end
 
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "Restaurant Saved!"
+      redirect_back restaurant_path
+    else
+      render 'restaurant/show'
+    end
+  end
 
+  def edit
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    if @restaurant.update(restaurant_params)
+      flash[:notice] = "Restaurant Updated!"
+      redirect_to @restaurant
+    else
+      redirect_back_or_to @restaurant
+    end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:alert] = "Restaurant Deleted!"
+    redirect_to '/restaurants'
 
 end
