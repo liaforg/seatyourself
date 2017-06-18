b528c07403b86a11d19e3c4a4d1c65693c3462c8
Moodiex0 <moodiex0@gmail.com>
2017-06-18 15:58:02 -0400

    Edit restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,39 +12,45 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
-  def restaurant_params
-    params.required(:restaurant).permit(:name, :url)
-  end
-
   def create
     @restaurant = Restaurant.create(restaurant_params)
     if @restaurant.save
       flash[:notice] = "Restaurant Saved!"
       redirect_back restaurant_path
     else
-      render 'restaurant/show'
+      flash.now[:error] = "Something went wrong, please try again!"
+      render :new
     end
   end
 
   def edit
-    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
-    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant = Restaurant.find(params[:id])
     if @restaurant.update(restaurant_params)
       flash[:notice] = "Restaurant Updated!"
-      redirect_to @restaurant
+      redirect_to restaurant_path
     else
+      flash.now[:error] = "Something went wrong, please try again!"
       redirect_back_or_to @restaurant
     end
   end
 
   def destroy
     @restaurant = Restaurant.find(params[:id])
-    @restaurant.destroy
-    flash[:alert] = "Restaurant Deleted!"
+    if @restaurant.destroy
+      flash[:alert] = "Restaurant Deleted!"
+      redirect_to root_path
+    else
+    flash.now[:error] = "Something went wrong, please try again!"
     redirect_to '/restaurants'
+    end
   end
-  
+
+  def restaurant_params
+    params.required(:restaurant).permit(:name, :url)
+  end
+
 end
